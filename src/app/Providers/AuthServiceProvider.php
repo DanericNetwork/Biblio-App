2be78b8a09cc344a8ddb287e5faac2cb6435ecc7
Tim Slager <timtendo12@gmail.com>
2023-12-08 10:05:02 +0100

    Added permissions and roles
    Added permissions and roles, They can be added to any instance by running the permissionSeeder.php. Also added a enum so we can easily name all permissions and roles by using either `PERMISSION::{{PERMISSION_NAME}}` or `ROLE::{{ROLE_NAME}}`. Also added a Auth gate for super admin that basically allows the super admin to have every permision.

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Enums\Roles;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Implicitly grant "Super Admin" role all permissions
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole(Roles::SUPERADMIN) ? true : null;
+        });
     }
 }
