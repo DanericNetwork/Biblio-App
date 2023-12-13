0a3f9bf0b9bfed7be43584807a8093b41a565c9b
Chris Friemann <github@chris.friemann.nl>
2023-12-13 14:48:19 +0100

    feat(WIP): item card skeleton

---
+++ src/resources/vue/components/ItemCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="item-card">
+        <p>Title of the item</p>
+        <span>Author</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ItemCard"
+    }
+</script>
+
+<style>
+
+</style>
