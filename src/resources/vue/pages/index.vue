e56c20667031b1dd436870cff340260d7426bcc7
Daneric <danericaalbers@hotmail.com>
2023-12-15 11:27:02 +0100

    chore: remove ItemCard component from index.vue

--- src/resources/vue/pages/index.vue
+++ src/resources/vue/pages/index.vue
@@ -1,20 +1,11 @@
 <template>
     <h1>Hello World!</h1>
-    <ItemCard title="My Title" description="My Description" warning="true" />
 </template>
 
 <script>
-import ItemCard from '../components/ItemCard.vue';
-
 export default {
-    name: "Index",
-    components: {
-        ItemCard
-    }
-}
-
+  name: "Index",
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
