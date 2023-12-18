c8b91085922a9f7f452f7772cdf9ed9747ddf270
Student Huib van der Gronde <hgronde1198@student.landstede.nl>
2023-12-18 13:29:30 +0100

    changes after review

--- src/resources/vue/pages/index.vue
+++ src/resources/vue/pages/index.vue
@@ -1,39 +1,12 @@
 <template>
-    <div class="gray">
-        <SearchBar></SearchBar>
-        <div class="spacer"></div>
-        <SearchButton></SearchButton>
-    </div>
+    
 </template>
 
 <script>
-import SearchBar from './components/SearchBar.vue';
-import SearchButton from './components/SearchButton.vue';
 
-export default {
-    components: {
-        SearchButton,
-        SearchBar
-},
-};
 
 </script>
 <style>
-.gray {
-    background-color: #f2f2f2;
-    padding: 20px;
-    border-radius: 5px;
-    width: 300px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-        
-}
-.spacer {
-    margin-bottom: 20px;
-}
-
 body {
     background-color: gray;
 }
