e985d8d558bf76f88302c0ef5031821aabf34dd7
Student Huib van der Gronde <hgronde1198@student.landstede.nl>
2023-12-18 11:29:22 +0100

    model gemaakt. alleen kleur moet nog aangepast worden

---
+++ src/resources/vue/components/model.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="box">
+        <svg class="close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="transform: rotate(45deg);"><path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path></svg>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .box {
+        background-color: darkblue;
+        width: 400px;
+        height: 400px;
+        position: relative;
+    }
+
+    .close-icon {
+        position: absolute;
+        height: 40px;
+        width: 40px;
+        top: 5px;
+        right: 5px;
+    }
+</style>
+    
--- src/resources/vue/pages/index.vue
+++ src/resources/vue/pages/index.vue
@@ -1,9 +1,18 @@
 <template>
     <h1>Hello World!</h1>
+    <model>
+        
+    </model>
 </template>
 
 <script>
+    import Model from '../components/model.vue';
 
+    export default {
+        components: {
+            Model
+        }
+    }
 </script>
 
 <style>
