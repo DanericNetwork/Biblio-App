f26781ee8c78d2db2f5ff1b510b08c3f3882afae
Chris Friemann <github@chris.friemann.nl>
2023-12-18 09:35:36 +0100

    refactor: make `searchButton` more global, its now `Button`

---
+++ src/resources/vue/components/Button.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="button" :class="{disabled : disabled}" :style="{ backgroundColor: color }" @click="buttonAction">
+	{{ content }}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Button',
+		props: {
+			color: {
+				type: String,
+				default: '#FF8C00',
+			},
+			disabled: {
+				type: Boolean,
+				default: false,
+			},
+			content: {
+				type: String,
+				default: 'Button',
+			},
+			buttonAction: {
+				type: Function,
+				default: () => {},
+			},
+		},
+	};
+</script>
+
+<style lang="scss">
+	.button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 206px;
+		height: 50px;
+		cursor: pointer;
+		border-radius: 12px;
+		font-size: 24px;
+		font-weight: 500;
+
+		&.disabled {
+			opacity: 0.5;
+			cursor: not-allowed;
+		}
+	}
+</style>
