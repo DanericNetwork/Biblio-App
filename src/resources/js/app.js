bac09309528792b355285b940ea3ce3b0c7f8cb8
Daneric <danericaalbers@hotmail.com>
2023-12-20 11:04:18 +0100

    feat: vue-toastification plugin

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -2,18 +2,21 @@ import './bootstrap';
 import { createApp, h } from 'vue'
 import { createPinia } from 'pinia'
 import { createInertiaApp } from '@inertiajs/vue3'
+import ToastPlugin from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import '../vue/scss/global.scss'
 import '../vue/scss/variables.scss'
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('../vue/pages/**/*.vue', { eager: true })
-        return pages[`../vue/pages/${name}.vue`]
-    },
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(createPinia())
-            .mount(el)
-    },
+  resolve: name => {
+    const pages = import.meta.glob('../vue/pages/**/*.vue', { eager: true })
+    return pages[`../vue/pages/${name}.vue`]
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(createPinia())
+      .use(ToastPlugin)
+      .mount(el)
+  },
 })
