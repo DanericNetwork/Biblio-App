55402f86213d3c4224f10b551b3132186aec316d
Student Huib van der Gronde <hgronde1198@student.landstede.nl>
2024-01-10 13:20:20 +0100

    changes after review

--- src/resources/vue/components/model.vue
+++ src/resources/vue/components/model.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="bg-modal" @click="toggleModal" v-if="showModal">
         <div v-if="showModal" class="modal" @click.stop>
-            <svg class="close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="transform: rotate(45deg);">
-                <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path>
-            </svg>
-            <!-- <p class="modal-content">This is some content in the modal.</p> -->
+            <Icon class="close-icon" icon="close" color="#ff0000" />
             <slot></slot>
         </div>
     </div>
 </template>
 
 <script>
+import Icon from './icon.vue';
 export default {
+    title: 'Modal',
+    components: {
+        Icon
+    },
     data() {
         return {
             showModal: true
@@ -25,38 +27,37 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss">
 .bg-modal {
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-    background-color: gray;
-}
-    .modal {
-        position: relative;
+        position: absolute;
+        top: 0;
+        left: 0;
         display: flex;
-        flex-direction: column;
         align-items: center;
         justify-content: center;
-        width: 80%;
-        /* width: 900px; */
-        height: 80%;
-        /* height: 550px; */
-        background-color: #1D243A;
-    }
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(128, 128, 128, 0.7);
 
-.close-icon {
-    position: absolute;
-    height: 40px;
-    width: 40px;
-    top: 5px;
-    right: 5px;
-    fill: #fff;
-}
+        .modal {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 80%;
+            height: 80%;
+            background-color: #1D243A;
+
+            .close-icon {
+                position: absolute;
+                height: 40px;
+                width: 40px;
+                top: 5px;
+                right: 5px;
+                fill: #fff;
+            }
+        }
+    }
 </style>
--- src/resources/vue/pages/index.vue
+++ src/resources/vue/pages/index.vue
@@ -1,8 +1,16 @@
 <template>
 	<h1>Hello World!</h1>
+  <model>
+    <p class="modal-content">This is some content in the modal.</p>
+  </model>
 </template>
 <script>
+import model from '../components/model.vue';
+
 export default {
   name: "Index",
+  components: {
+    model
+  },
 };
 </script>
