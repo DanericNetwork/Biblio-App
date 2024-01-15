8445272fbc09d7b387dca7a51357e7507d985bff
Tim Slager <timtendo12@gmail.com>
2024-01-15 13:08:43 +0100

    item search endpoint

---
+++ src/app/Http/Requests/SearchItemRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Item;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchItemRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $user = auth()->user();
+        return $user->can('search', Item::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'search' => 'required|string|max:255'
+        ];
+    }
+}
