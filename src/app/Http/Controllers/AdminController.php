3d4f65cdf14e0e96ac68110e936ada6ce696efbf
Chris Friemann <github@chris.friemann.nl>
2024-01-17 14:47:48 +0100

    feat: return page with mock data

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -9,6 +9,24 @@ class AdminController extends Controller
 {
   public function index()
   {
-    return Inertia::render('admin/index');
+    return Inertia::render('admin/index', [
+      'userData' => [
+        'name' => 'John Doe',
+      ],
+      'count' => [
+        'items' => 10,
+        'customers' => 20,
+        'reservations' => 30,
+        'invoices' => 40,
+      ],
+      'chart1' => [
+        'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+        'series' => [1, 2, 3, 4, 5, 6, 7, 8]
+      ],
+      'chart2' => [
+        'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+        'series' => [0, 1, 2, 3, 4, 5, 6, 7]
+      ],
+    ]);
   }
 }
