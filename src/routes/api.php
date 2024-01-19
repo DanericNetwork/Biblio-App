2defd89e9643a88b3b3b3c75de846f4c218014a3
Chris Friemann <github@chris.friemann.nl>
2024-01-19 09:28:38 +0100

    feat: add post to API

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/item/search', [ItemController::class, 'search']);
+Route::post('/users/search', [UserController::class, 'search']);
